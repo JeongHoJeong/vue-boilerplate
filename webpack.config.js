f5077ef847c2f47b41887e93c0c6240b4da798cf
Jeongho Jeong <fiil12@hotmail.com>
2017-03-19 17:42:24 +0900

    detach web build

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,19 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const config = require(path.resolve(__dirname, 'webpack.base.config.js'))
 
-module.exports = {
-  entry: path.resolve(__dirname, 'app/index.ts'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    modules: [
-      path.resolve(__dirname, 'app/components'),
-      path.resolve(__dirname, 'app'),
-      'node_modules'
-    ],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['.js', '.vue', '.ts']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            appendTsSuffixTo: [/\.vue$/]
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.vue$/,
-        use: {
-          loader: 'vue-loader',
-          options: {
-            loaders: {
-              js: 'ts-loader'
-            }
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            'css-loader?importLoaders=1',
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: function () {
-                  return [
-                    require('precss'),
-                    require('autoprefixer')
-                  ]
-                }
-              }
-            },
-            'sass-loader'
-          ]
-        })
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'app/index.html')
-    })
-  ]
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const plugins = config.plugins || []
+
+plugins.push(new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, 'app/index.html')
+}))
+
+config.plugins = plugins
+
+config.output = {
+  path: path.resolve(__dirname, 'dist/web'),
+  filename: 'bundle.js'
 }
+
+module.exports = config

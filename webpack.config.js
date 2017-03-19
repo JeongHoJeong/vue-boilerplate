afbdaba93a61e2a19b3a7b8f4a7ff7915baf5af3
Jeongho Jeong <fiil12@hotmail.com>
2017-03-19 22:30:09 +0900

    integrate webpack configs into one file

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,63 @@
 const path = require('path')
 const config = require(path.resolve(__dirname, 'webpack.base.config.js'))
-
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-config.entry = path.resolve(__dirname, 'app/index.ts')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
 
 const plugins = config.plugins || []
 
-plugins.push(new HtmlWebpackPlugin({
-  template: path.resolve(__dirname, 'app/index.html')
-}))
+if (!process.env.TARGET || process.env.TARGET === 'web') {
+  console.log("Build target: Web")
+
+  config.entry = path.resolve(__dirname, 'app/index.ts')
+
+  config.output = {
+    path: path.resolve(__dirname, 'dist/web'),
+    filename: 'bundle.js'
+  }
+
+  plugins.push(new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'app/index.html')
+  }))
+} else if (process.env.TARGET === 'electron') {
+  console.log("Build target: Electron")
+
+  config.entry = {
+    main: path.resolve(__dirname, 'app/electron.js'),
+    bundle: path.resolve(__dirname, 'app/index.ts')
+  }
+
+  config.output = {
+    path: path.resolve(__dirname, 'dist/electron'),
+    filename: '[name].js'
+  }
+
+  plugins.push(new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'app/index.electron.html'),
+    inject: false
+  }))
+
+  config.module.noParse = /electron\.js$/
+} else {
+  throw new Error("Invalid build target.")
+}
 
 config.plugins = plugins
 
-config.output = {
-  path: path.resolve(__dirname, 'dist/web'),
-  filename: 'bundle.js'
+if (process.env.PROD || process.env.PRODUCTION) {
+  console.log("Build mode: Production")
+
+  config.plugins = config.plugins || []
+
+  config.plugins.push(new webpack.DefinePlugin({
+    PRODUCTION: JSON.stringify(true),
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  }))
+  config.plugins.push(new UglifyJsPlugin())
+} else {
+  console.log("Build mode: Development")
 }
 
 module.exports = config

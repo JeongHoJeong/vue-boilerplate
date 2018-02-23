95ad511a643e50a3447734cd9e12e460f0576d01
Jeongho Jeong <fiil12@hotmail.com>
2018-02-24 03:10:23 +0900

    use single quote

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,12 @@
-import Vue from "vue"
+import Vue from 'vue'
 
-import router from "router"
-import store from "store"
-import "style.scss"
+import router from './router'
+import store from './store'
+import './style.scss'
 
 const app = new Vue({
-  el: "#app",
-  template: "<router-view></router-view>",
+  el: '#app',
+  template: '<router-view></router-view>',
   router,
   store,
 })

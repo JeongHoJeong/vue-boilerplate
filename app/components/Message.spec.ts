4a079d4b0c52c2d586822d417afc095fa550b8ce
Jeongho Jeong <fiil12@hotmail.com>
2018-03-14 04:34:39 +0900

    update Message.spec according to the new spec

--- app/components/Message.spec.ts
+++ app/components/Message.spec.ts
@@ -1,31 +1,15 @@
-import { createLocalVue, shallow } from '@vue/test-utils'
-import VueRouter from 'vue-router'
+import { shallow } from '@vue/test-utils'
 import Message from './Message.vue'
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
-
-const router = new VueRouter({})
-
 describe('Message', () => {
-  const wrapper = shallow(Message, {
-    localVue,
-    router,
-  })
+  const wrapper = shallow(Message)
   const vm = wrapper.vm
 
   wrapper.setProps({
-    link: 'test-link',
     message: 'test-message',
   })
 
   it('displays message', () => {
     expect(wrapper.find('p').text()).toBe('test-message')
-  })
-
-  it('moves to link', () => {
-    wrapper.trigger('click')
-
-    expect(vm.$route.path).toBe('/test-link')
   })
 })

3f1851bc3ca935a8d1c8b68721c325edda88a559
Jeongho Jeong <fiil12@hotmail.com>
2018-03-14 04:32:07 +0900

    add CounterPage component

---
+++ app/components/CounterPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <page :centered="true">
+    <counter></counter>
+  </page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import Counter from './Counter.vue'
+import Page from './Page.vue'
+
+@Component({
+  components: {
+    Counter,
+    Page,
+  },
+})
+export default class CounterPage extends Vue {
+}
+</script>
